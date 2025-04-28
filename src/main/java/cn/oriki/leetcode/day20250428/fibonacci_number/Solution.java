a2f8d874d70fbfe2b598933156cc99da7cf902e3
oriki <n0rk1n@live.cn>
2025-04-28 11:45:06 +0800

    509. 斐波那契数 ✅

---
+++ src/main/java/cn/oriki/leetcode/day20250428/fibonacci_number/Solution.java
@@ -0,0 +1,23 @@
+package cn.oriki.leetcode.day20250428.fibonacci_number;
+
+/**
+ * <a href="https://leetcode.cn/problems/fibonacci-number/">509. 斐波那契数</a>
+ */
+public class Solution {
+
+    public int fib(int n) {
+        // f(0) = 0
+        // f(1) = 1
+        if (n < 2) {
+            return n;
+        }
+        int p, q = 0, r = 1;
+        for (int i = 2; i <= n; ++i) { // example: n = 2，执行一次，取 r 的值
+            // 非递归方式
+            p = q;
+            q = r;
+            r = p + q;
+        }
+        return r;
+    }
+}
