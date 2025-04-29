61348d0666ffdc89aed105cc542fc10d5e196c44
oriki <n0rk1n@live.cn>
2025-04-29 10:44:58 +0800

    35. 搜索插入位置 ✅

---
+++ src/main/java/cn/oriki/leetcode/day20250429/search_insert_position/Solution.java
@@ -0,0 +1,22 @@
+package cn.oriki.leetcode.day20250429.search_insert_position;
+
+/**
+ * <a href="https://leetcode.cn/problems/search-insert-position/">35. 搜索插入位置</a>
+ */
+public class Solution {
+
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (right + left) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
