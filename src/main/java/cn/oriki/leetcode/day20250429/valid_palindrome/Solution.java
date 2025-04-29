67e912a8040e89a411e8ac3930c679118b0463cf
oriki <n0rk1n@live.cn>
2025-04-29 10:28:15 +0800

    125. 验证回文串 ✅

---
+++ src/main/java/cn/oriki/leetcode/day20250429/valid_palindrome/Solution.java
@@ -0,0 +1,24 @@
+package cn.oriki.leetcode.day20250429.valid_palindrome;
+
+/**
+ * <a href="https://leetcode.cn/problems/valid-palindrome/">125. 验证回文串</a>
+ */
+public class Solution {
+
+    public boolean isPalindrome(String s) {
+        // 移出所有的非字母数字字符，并且将字母转换为小写
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            // 回文串可以理解为对称位置的字符是相同的，如果不相同就返回 false
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
