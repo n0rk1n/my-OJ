0c73623c2caad58e858d314269e1a4477d44ae42
oriki <n0rk1n@live.cn>
2025-05-04 15:59:48 +0800

    0. 将解法整理到 SNIP 中

--- src/main/java/cn/oriki/leetcode/day20250504/delete_duplicate_emails/Solution.sql
+++ src/main/java/cn/oriki/leetcode/day20250504/delete_duplicate_emails/Solution.sql
@@ -1,3 +1,4 @@
 -- 196. 删除重复的电子邮箱
 -- https://leetcode.cn/problems/delete-duplicate-emails/
+-- TO SNIP
 delete from Person where id not in (select id from (select min(id) id from Person group by email) p)
